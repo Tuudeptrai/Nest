72fa09123330f549bfd9d2fe2e34449bcc01a594
Vu Van Tuu <vuvantuu.gl@gmail.com>
2024-01-07 14:34:07 +0700

    MOGO DB CONNECT

--- README.md
+++ README.md
@@ -7,5 +7,15 @@ Set-ExecutionPolicy RemoteSigned
 nest generate controller user
 nest g resource user
 npm i --save-exact ejs@3.1.9
+docker pull mongo
+docker run --name mymongodb -d -p 27018:27017 mongo:latest
+docker start mymongodb
+docker container ls
+npm install --save @nestjs/mongoose mongoose
+
+imports: [MongooseModule.forRoot('mongodb://localhost/nest')], add to app.module.ts
+npm i dotenv@16.0.3
+npm i --save-exact @nestjs/config@2.3.1
+
 =================
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  // imports: [MongooseModule.forRoot('mongodb://localhost:27018'),ConfigModule.forRoot({
+  //   isGlobal: true,
+  // })],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+        isGlobal: true,
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+
+
+}
